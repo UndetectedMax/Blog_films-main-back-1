008074dac591368b6baa8afd476677f4a4a5b0f2
UndetectedMax <maxm13052004@gmail.com>
2023-11-22 20:01:31 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -11,20 +12,23 @@ const User = require('./models/User');
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
 
-mongoose.connect(keys.mongoURI)
-    .then(() => console.log('MongoDB connected.'))
-    .catch((err) => console.log(err));
-
 const app = express();
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(keys.mongoURI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(clientPath));
 app.use('/api/post', postRouter);
 app.use('/api/auth', authRouter);
-
-app.listen(port, () => {
-    console.log(`Server has been started on port ${port}`);
-});
 
 app.get('/users', async (req, res) => {
     try {
@@ -70,3 +74,9 @@ app.post('/assignRole/:userId/:roleId', async (req, res) => {
         res.status(500).json({ message: 'Ошибка при изменении роли пользователя' });
     }
 });
+
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log("listening for requests on port localhost:5000");
+    })
+})
